0e01b6aebd90dc9daec54d312cab1b3a69a78d56
jks6404 <jannusingh.75@gmail.com>
2023-11-13 21:30:17 +0530

    finale

--- Startquiz/script.js
+++ Startquiz/script.js
@@ -5,7 +5,7 @@ const section2Btn = document.getElementById('section2-button');
 const section1Btn = document.getElementById('section1-button');
 
 section2Btn.addEventListener('click',()=>{
-    location.href='../drag&drop/index.html';
+    location.href='../Drag&Drop/index.html';
 })
 
 section1Btn.addEventListener('click',()=>{
